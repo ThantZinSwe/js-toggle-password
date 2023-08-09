261ac7ffd3d7dcdd7e3c11ae089cd0a76f8b9ca5
thantzinswe <thantzinswe01@gmail.com>
2023-08-10 00:23:37 +0630

    change function

--- src/index.js
+++ src/index.js
@@ -1,56 +1,98 @@
-import { applyStyles, createSvgElement } from "./style.js";
+import { createStyles, createSvgElement } from "./style.js";
+import { styles, position, defaults, custom } from "./property.js";
 
-function togglePassword(
-  position = {
-    right: 8,
-    vertical: 50,
-  },
-  svg = {
-    width: 24,
-    height: 24,
-    color: "#4b5563",
-  },
-  customSvg = {
-    visible: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-      </svg>`,
-    invisible: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
-      </svg>
-    `,
-  }
-) {
-  const passwordFields = document.querySelectorAll("input[type=password]");
+function togglePassword() {
+  const toggleSvg = {
+    /**
+     * Define toggle password svg styles function for modify svg style.
+     *
+     * @param {number} width - Width must be pixels (px)
+     * @param {number} height - Height must be pixels (px)
+     * @param {string} color - Color must be hex color code
+     *
+     * @returns {toggleSvg} - Returns the toggleSvg object for method chaining.
+     */
+    styles: ({ width, height, color } = {}) => {
+      styles.width = width;
+      styles.height = height;
+      styles.color = color;
 
-  applyStyles(svg, position);
+      return toggleSvg;
+    },
 
-  passwordFields.forEach((passwordField) => {
-    const parentElement = passwordField.parentElement;
-    parentElement.classList.add("tp-blk");
+    /**
+     * Define toggle password svg position function for modify svg position.
+     *
+     * @param {number} right - Right must be pixels (px)
+     * @param {string} top - Top must be percentage
+     *
+     * @returns {toggleSvg} - Returns the toggleSvg object for method chaining.
+     */
+    position: ({ right, top } = {}) => {
+      position.right = right;
+      position.top = top;
 
-    const toggleSvg = document.createElement("span");
-    toggleSvg.classList.add("tp-position");
+      return toggleSvg;
+    },
 
-    const visibleSvg = createSvgElement(customSvg.visible);
-    const inVisibleSvg = createSvgElement(customSvg.invisible);
+    /**
+     * Define toggle password custom svg for visible and invisible.
+     *
+     * @param {string} visibleSvg - Custom SVG for the visible state and must be svg
+     * @param {string} inVisibleSvg - Custom SVG for the invisible state and must be svg
+     *
+     * @returns {toggleSvg} - Returns the toggleSvg object for method chaining.
+     */
+    custom: ({ visibleSvg, inVisibleSvg } = {}) => {
+      custom.visibleSvg = visibleSvg;
+      custom.inVisibleSvg = inVisibleSvg;
 
-    const updatePasswordToggle = () => {
-      const svg = toggleSvg.querySelector("svg");
-      if (passwordField.type === "password") {
-        passwordField.type = "text";
-        toggleSvg.replaceChild(inVisibleSvg.cloneNode(true), svg);
-      } else {
-        passwordField.type = "password";
-        toggleSvg.replaceChild(visibleSvg.cloneNode(true), svg);
-      }
-    };
+      return toggleSvg;
+    },
 
-    toggleSvg.appendChild(visibleSvg.cloneNode(true));
-    parentElement.appendChild(toggleSvg);
+    /**
+     * Create and apply toggle when click svg at input type password field.
+     */
+    create: () => {
+      const passwordFields = document.querySelectorAll("input[type=password]");
 
-    toggleSvg.addEventListener("click", updatePasswordToggle);
-  });
+      passwordFields.forEach((passwordField) => {
+        const parentElement = passwordField.parentElement;
+        parentElement.classList.add("js-toggle-password-blk");
+
+        createStyles(styles, position);
+
+        const toggleSvg = document.createElement("span");
+        toggleSvg.classList.add("js-toggle-password-position");
+
+        let visibleSvg = custom.visibleSvg
+          ? createSvgElement(custom.visibleSvg)
+          : createSvgElement(defaults.visibleSvg);
+
+        let inVisibleSvg = custom.inVisibleSvg
+          ? createSvgElement(custom.inVisibleSvg)
+          : createSvgElement(defaults.inVisibleSvg);
+
+        const updatePasswordToggle = () => {
+          const svg = toggleSvg.querySelector("svg");
+          if (passwordField.type === "password") {
+            passwordField.type = "text";
+            toggleSvg.replaceChild(visibleSvg.cloneNode(true), svg);
+          } else {
+            passwordField.type = "password";
+            toggleSvg.replaceChild(inVisibleSvg.cloneNode(true), svg);
+          }
+        };
+
+        toggleSvg.appendChild(inVisibleSvg.cloneNode(true));
+        parentElement.appendChild(toggleSvg);
+
+        toggleSvg.addEventListener("click", updatePasswordToggle);
+      });
+    },
+  };
+
+  return toggleSvg;
 }
 
 export default togglePassword;
--- src/style.js
+++ src/style.js
@@ -1,35 +1,45 @@
-function applyStyles(svg, position) {
+/**
+ * Create and apply dynamic styles for the password toggle functionality.
+ *
+ * @param {object} styles - style properties (width, height, color).
+ * @param {object} position - position properties (right, top).
+ */
+function createStyles(styles, position) {
   const style = document.createElement("style");
   style.textContent = `
-      .tp-blk {
-        position: relative;
-      }
-  
-      .tp-svg {
-        width: ${svg.width}px;
-        height: ${svg.height}px;
-        color: ${svg.color};
-      }
-  
-      .tp-position {
-        position: absolute;
-        right: ${position.right}px;
-        top: ${position.vertical}%;
-        transform: translateY(-50%);
-        cursor: pointer;
-      }
-    `;
+    .js-toggle-password-blk {
+      position: relative;
+    }
+    .js-toggle-password-svg {
+      width: ${styles.width}px;
+      height: ${styles.height}px;
+      color: ${styles.color};
+    }
+    .js-toggle-password-position {
+      position: absolute;
+      right: ${position.right ?? 8}px;
+      top: ${position.top ?? 50}%;
+      transform: translateY(-50%);
+      cursor: pointer;
+  }`;
+
   document.head.appendChild(style);
 }
 
+/**
+ * Create svg element from string paramemter svg and add necessary svg class
+ *
+ * @param {string} svg
+ * @returns {svgElement} - Created svg element
+ */
 function createSvgElement(svg) {
   const parser = new DOMParser();
   const svgElement = parser
     .parseFromString(svg, "image/svg+xml")
     .querySelector("svg");
-  svgElement.classList.add("tp-svg");
+  svgElement.classList.add("js-toggle-password-svg");
 
   return svgElement;
 }
 
-export { applyStyles, createSvgElement };
+export { createStyles, createSvgElement };
